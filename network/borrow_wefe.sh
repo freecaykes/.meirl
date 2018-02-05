bfa703b474762ef64f533d4ac7089838e58b951d
ec-io <ecandra@protonmail.com>
2018-02-05 00:17:46 -0800

    i3 pulseaudio on start

--- network/borrow_wefe.sh
+++ network/borrow_wefe.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
-echo iwconfig
+LAN_CARD="wlp3s0"
+
+output=$(echo -e sudo iwlist $LAN_CARD scan | grep ESSID)
+n= $( $output | wc -l)
+
+echo -e $output
+
+read -p "Enter choice [1-$n] " $ssid
+read -p "Enter key for $ssid" $password
+iwconfig $ essid $ssid key $password
